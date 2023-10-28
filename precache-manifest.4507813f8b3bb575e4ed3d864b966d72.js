303906ab4df1385dd74c3b977cbe8e20ed6d4fb9
Joaquin-Feijoo <joaquinfeijoo140@gmail.com>
2023-10-27 23:28:59 -0300

    Updates

---
+++ precache-manifest.4507813f8b3bb575e4ed3d864b966d72.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e2e444badf1e7b2d8b8634957165c90",
+    "url": "/UnaNocheEnLaTecnica/index.html"
+  },
+  {
+    "revision": "229f291dbb85ada2d02b",
+    "url": "/UnaNocheEnLaTecnica/static/css/2.dcb2f796.chunk.css"
+  },
+  {
+    "revision": "391418c8e205859c7544",
+    "url": "/UnaNocheEnLaTecnica/static/css/main.1a4b1e9d.chunk.css"
+  },
+  {
+    "revision": "229f291dbb85ada2d02b",
+    "url": "/UnaNocheEnLaTecnica/static/js/2.f74a0c3e.chunk.js"
+  },
+  {
+    "revision": "d06d8fe717dc215d2ddf",
+    "url": "/UnaNocheEnLaTecnica/static/js/3.71e6714e.chunk.js"
+  },
+  {
+    "revision": "391418c8e205859c7544",
+    "url": "/UnaNocheEnLaTecnica/static/js/main.e83b709b.chunk.js"
+  },
+  {
+    "revision": "df7dac0a87e1301d7c86",
+    "url": "/UnaNocheEnLaTecnica/static/js/runtime~main.25a00472.js"
+  },
+  {
+    "revision": "536e67f251ce4ed8efe86f0a5e953d2a",
+    "url": "/UnaNocheEnLaTecnica/static/media/NightsideDemoRegular.536e67f2.ttf"
+  },
+  {
+    "revision": "3d76d676d664d4bee0598c3ef62d96eb",
+    "url": "/UnaNocheEnLaTecnica/static/media/TheDeathDog.3d76d676.ttf"
+  },
+  {
+    "revision": "74f5864dcaf93e8425209d96867600b6",
+    "url": "/UnaNocheEnLaTecnica/static/media/image1.74f5864d.png"
+  },
+  {
+    "revision": "3b061cf4a085f98f6ce73444424e4a67",
+    "url": "/UnaNocheEnLaTecnica/static/media/image2.3b061cf4.png"
+  },
+  {
+    "revision": "89a7eb850003fa66874b5dc155bfe46e",
+    "url": "/UnaNocheEnLaTecnica/static/media/image3.89a7eb85.png"
+  },
+  {
+    "revision": "fd0b8faf083a125ade0dfab2bd7a99d6",
+    "url": "/UnaNocheEnLaTecnica/static/media/image4.fd0b8faf.png"
+  },
+  {
+    "revision": "10790790f6de16ca1ee6c5a78d14558f",
+    "url": "/UnaNocheEnLaTecnica/static/media/image5.10790790.png"
+  }
+]);
